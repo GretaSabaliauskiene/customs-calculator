57ed8f085b0e999fc52ab2334e5337807202bf9d
Greta <sabaliauskiene@yahoo.com>
2021-04-19 17:48:19 +0300

    chore: added maxlength validation

--- js/calculator-validation.js
+++ js/calculator-validation.js
@@ -4,57 +4,67 @@ $(document).ready(function() {
       product_price : {
         required: true,
         minlength: 1,
+        maxlength:10,
         number: true,
-        min: 0
+        min: 0,
       },
       transfer_price_before_eu: {
         required: true,
         minlength: 1,
+        maxlength:10,
         number: true,
-        min: 0
+        min: 0,
       },
       customs_procent: {
         required: true,
         minlength: 1,
+        maxlength:10,
         number: true,
-        min: 0
+        min: 0,
       },
       transfer_price_in_eu: { 
         required: true,
         minlength: 1,
+        maxlength:10,
         number: true,
-        min: 0
+        min: 0,
       },
       other_costs: { 
         minlength: 1,
+        maxlength:10,
         number: true,
-        min: 0
+        min: 0,
       },
     },
     messages : {
       product_price : {
         required: "Įveskite prekės kainą",
         number: "Įveskite validų skaičių",
-        min: "Įveskite sveiką skaičių"
+        min: "Įveskite sveiką skaičių",
+        maxlength: "Įvestas skaičius neturėtų viršyti 10 skaitmenų"
       },
       transfer_price_before_eu: {
         required: "Įveskite atgabenimo iki EU sienos kainą",
         number: "Įveskite validų skaičių",
-        min: "Įveskite sveiką skaičių"
+        min: "Įveskite sveiką skaičių",
+        maxlength: "Įvestas skaičius neturėtų viršyti 10 skaitmenų"
       },
       customs_procent: {
         required: "Įveskite muito normą",
         number: "Įveskite validų skaičių",
-        min: "Įveskite sveiką skaičių"     
+        min: "Įveskite sveiką skaičių"    ,
+        maxlength: "Įvestas skaičius neturėtų viršyti 10 skaitmenų" 
       },
       transfer_price_in_eu: {
         required: "Įveskite atgabenimo nuo EU sienos iki išmuitinimo vietos kainą",
         number: "Įveskite validų skaičių",
-        min: "Įveskite sveiką skaičių"   
+        min: "Įveskite sveiką skaičių"   ,
+        maxlength: "Įvestas skaičius neturėtų viršyti 10 skaitmenų"
       },
       other_costs: {
         number: "Įveskite validų skaičių",
-        min: "Įveskite sveiką skaičių"   
+        min: "Įveskite sveiką skaičių",
+        maxlength: "Įvestas skaičius neturėtų viršyti 10 skaitmenų"   
       }
     }
   });
